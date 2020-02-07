f586a29ac4a5d34723b9d158376254ad75c4ec9f
wanghao0911 <1011954307@qq.com>
2020-02-07 11:23:11 +0800

    init

---
+++ src/main/java/cn/roboteco/App.java
@@ -0,0 +1,13 @@
+package cn.roboteco;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
